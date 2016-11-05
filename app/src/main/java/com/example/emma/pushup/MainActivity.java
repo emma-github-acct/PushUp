98c7b0193c6ff061356d9386d013c7b44f089649
Emma <emmalysek@gmail.com>
2016-11-05 15:48:42 -0400

    Adds TextView "FooBar"

--- app/src/main/java/com/example/emma/pushup/MainActivity.java
+++ app/src/main/java/com/example/emma/pushup/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -61,4 +62,6 @@ public class MainActivity extends AppCompatActivity {
         int add = i + ii;
         int sub = i - ii;
     }
+
+
 }
